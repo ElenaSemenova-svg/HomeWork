76899a7c2f3088fcee923bbefb83740e3ab8e4ba
Elena Semenova <leo110274@yandex.ru>
2023-01-31 20:31:17 +0300

    add HomeWork1: 4 programs C#

---
+++ Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+//a = 5; b = 7->max = 7
+//a = 2 b = 10->max = 10
+//a = -9 b = -3->max = -3
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine()??"");
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine()??"");
+
+/*
+Console.WriteLine("Введите первое число: ");
+string numberStr1 = Console.ReadLine()??"";
+int number1 = Convert.ToInt32(numberStr1);
+Console.WriteLine("Введите второе число: ");
+string numberStr2 = Console.ReadLine()??"";
+int number2 = Convert.ToInt32(numberStr2);*/
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Число {number1} больше числа {number2}");
+}
+else 
+{
+    Console.WriteLine($"Число {number2} больше числа {number1}");
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.WriteLine("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine()??"");
+//int Number = Math.Abs(int.Parse(Console.ReadLine()??"")); //преобразуем тип данных в инт и делаем число положительным
+if (Number >= 1)
+for (int i = 1; i <= Number; i++)
+{
+    if (i%2 == 0) 
+    {
+    Console.Write($" {i}");
+    }
+}
+
+else Console.WriteLine("Введите число больше 1");
+
